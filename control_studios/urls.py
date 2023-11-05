016527dcfef1734f42eb606e0a81f5b6c7cef8df
RuffoloL <luciano.ruffolo@hotmail.com.ar>
2023-11-05 15:17:10 -0300

    Mejoras a la organizacion, movimos URL y template a la app control_studios

---
+++ control_studios/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from control_studios.views import listar_estudiantes
+
+#Estas son las URLS de la app control_studios
+
+urlpatterns = [
+    path("estudiantes/", listar_estudiantes)
+]
