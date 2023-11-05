5fcbd9cdc1eb9d451e0423c308fd047e31ae7401
RuffoloL <luciano.ruffolo@hotmail.com.ar>
2023-11-05 14:38:28 -0300

    Agregada vista y template para lista estudiantes

--- control_studios/views.py
+++ control_studios/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+def listar_estudiantes(request):
+    contexto = {
+        "profesor": "Pedro",
+        "estudiantes": [
+            {"nombre": "Erik", "apellido": "Harmitton", "nota": 9},
+            {"nombre": "Nicolas", "apellido": "Lanvers", "nota": 8},
+            {"nombre": "Sebastian", "apellido": "Antonini", "nota": 10},
+            {"nombre": "Mariano", "apellido": "Martina", "nota": 7},  
+        ]
+    }
+    http_response = render(
+        request=request,
+        template_name='lista_estudiantes.html',
+        context=contexto,
+    )
+    return http_response
