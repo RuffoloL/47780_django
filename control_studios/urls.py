0a5250bf813146c4a0ee3f2984575bf18c5a19ac
RuffoloL <luciano.ruffolo@hotmail.com.ar>
2023-11-07 20:11:50 -0300

    Agregada lista y formulario de cursos

--- control_studios/urls.py
+++ control_studios/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from control_studios.views import listar_estudiantes
+from control_studios.views import listar_estudiantes, listar_cursos, crear_curso
 
 #Estas son las URLS de la app control_studios
 urlpatterns = [
     path("estudiantes/", listar_estudiantes, name="lista_estudiantes"),
+    path("cursos/", listar_cursos, name="lista_cursos"),
+    path("crear-curso/", crear_curso, name="crear_curso"),
 ]
--- control_studios/views.py
+++ control_studios/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
-from control_studios.models import Estudiante
+from control_studios.models import Estudiante, Curso
 
 def listar_estudiantes(request):
     contexto = {
@@ -13,3 +14,32 @@ def listar_estudiantes(request):
         context=contexto,
     )
     return http_response
+
+def listar_cursos(request):
+    contexto = {
+        "cursos": Curso.objects.all(),
+    }
+    http_response = render(
+        request=request,
+        template_name='control_studios/lista_cursos.html',
+        context=contexto,
+    )
+    return http_response
+
+
+def crear_curso(request):
+    if request.method == "POST":
+        data = request.POST
+        curso = Curso(nombre=data['nombre'], comision=data['comision'])
+        curso.save()
+        url_exitosa = reverse('lista_cursos')
+        return redirect(url_exitosa)
+    else:
+        http_response = render(
+            request=request,
+            template_name='control_studios/formulario_curso_a_mano.html',
+    )
+    return http_response
+    
+        
+    
